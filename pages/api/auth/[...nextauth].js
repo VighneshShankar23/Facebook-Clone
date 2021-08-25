6799b52fac51b770c743cb5b15fce84deb480fcc
Vighnesh Shankar <vighneshshankar23@gmail.com>
2021-08-25 23:00:07 +0530

    Added google auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,9 +3,9 @@ import Providers from "next-auth/providers";
 
 export default NextAuth({
   providers: [
-    Providers.Facebook({
-        clientId: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+    Providers.Google({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
 }),
   ],
 });
